1c35e537a0f9a7992afe7b7bcf52f6e1ce745e24
Grechemax <grech@i.ua>
2018-10-07 10:38:17 +0300

    added sign in functionality

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,25 @@
+import * as firebase from 'firebase';
+import {Injectable} from "@angular/core";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  signupUser(email: string, password: string) {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .catch(
+        error => console.log(error)
+      );
+  }
+
+  signinUser(email: string, password: string) {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(
+        response => console.log(response)
+      )
+      .catch(
+        error => console.log(error)
+      )
+
+  }
+}
